692cd23eb48c1e01e0f1e830b896342594e2bdea
Alan Cesar Pires <alancpires01@gmail.com>
2020-11-20 16:18:51 -0300

    #1: creating flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
+    @app.route('/')
+    def index():
+        return {"status": "ok"}
 
-@app.route('/')
-def index():
-    return 'Oi'
+    return app
